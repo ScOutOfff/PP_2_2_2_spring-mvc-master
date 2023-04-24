5c62536e5c5fa105b26f8701664d2a1baf4b1368
Nikita <125838286+ScOutOfff@users.noreply.github.com>
2023-04-24 18:18:38 +0300

    24.04.23|||||18:20

--- PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
+++ PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
@@ -4,12 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
+import web.CarService;
+import web.CarServiceImp;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class CarController {
@@ -17,26 +15,11 @@ public class CarController {
     @GetMapping(value = "/cars")
     public String printCar(HttpServletRequest request, ModelMap model) {
         String count = request.getParameter("count");
-        System.out.println("count = " + count);
+//        System.out.println("count = " + count);
 
-        List<Car> carList = new ArrayList<>();
-        carList.add(new Car("Audi", 630, 4123));
-        carList.add(new Car("BMW", 512, 1242));
-        carList.add(new Car("MERS", 523, 5132));
-        carList.add(new Car("Lada", 2, 4623));
-        carList.add(new Car("Toyota", 562, 5134));
+        CarService carServiceImp = new CarServiceImp();
 
-        if (!(count == null)) {
-            int i = Integer.parseInt(count);
-            if(i>5) i = 5;
-            List<Car> cars = new ArrayList<>();
-            for (int j = 0; j < i; j++) {
-                cars.add(carList.get(j));
-            }
-            model.addAttribute("cars", cars);
-        } else {
-            model.addAttribute("cars", carList);
-        }
+        model.addAttribute("cars", carServiceImp.getCarList(count));
 
         return "cars";
     }
